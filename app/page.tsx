7bf2a4a133c746faeaa55d76a93068d2662bf5eb
KikiMaouw <cristelle.hermand@gmail.com>
2025-01-29 14:57:44 +0100

    Add Footer in Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
     <main>
       <h1>Home Page</h1>
       <Header />
+      <Footer />
     </main>
   );
 }
