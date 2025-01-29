a9b9bca57062dc4d931eda9115a321866f43691f
Neogiciaa <chevalier.stepheh@hotmail.com>
2025-01-29 10:31:23 +0100

    First commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <h1>Ceci est mon header</h1>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/app/components/Header";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <Header />
+    </main>
+  );
+}
