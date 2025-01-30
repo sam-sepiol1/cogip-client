f35266cb3ed4e75bdb8717be2fc7b3a0f37fc5fb
KikiMaouw <cristelle.hermand@gmail.com>
2025-01-30 11:50:36 +0100

    Added a Footer component on the main

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Contacts from "../components/Contacts";
 
 export default function Invoices() {
     return (
         <main>
             <Header />
+            <Contacts />
+            <Footer />
         </main>
     );
 }
