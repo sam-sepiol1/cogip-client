067a97b8d0b827dda9bdbc876cd5c999dd311e02
KikiMaouw <cristelle.hermand@gmail.com>
2025-01-30 09:58:16 +0100

    Removed <div> and add a border  on <footer> with 2px height and same color

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,37 +8,45 @@ export default function Footer() {
     const pathname = usePathname();
 
     return (
-        <footer className="w-[75.76%] mx-auto flex flex-col items-center justify-start mb-[100.5px]">
-            <div className="w-full h-[2px] bg-[#F9DE4E]"></div>
+        <footer className="w-[75.76%] mx-auto flex flex-col items-center justify-start mb-[100px] border-t-2 border-[#F9DE4E]">
             <div className="w-full flex justify-between items-center mt-6">
                 <h2
                     className="footer-logo text-5xl font-bold text-[#434138] border-4 ml-4 px-1.5 py-0.5"
-                    style={{ borderColor: "#F9DE4E" }}>
+                    style={{borderColor: "#F9DE4E"}}>
                     COGIP
                 </h2>
                 <div className="w-[460px] h-[128px] flex flex-col items-start mt-6 text-left">
                     <div className="flex items-center space-x-2">
-                        <Image src="/Footer/round-place-24px.png" alt="Location" width={24} height={24} className="shrink-0"/>
+                        <Image src="/Footer/round-place-24px.png" alt="Location" width={24} height={24}
+                               className="shrink-0"/>
                         <p className="footer-nav text-[#434138] mb-2">
                             Square des Martyrs, 6000 Charleroi
                         </p>
                     </div>
                     <div className="flex items-center mt-2 space-x-2">
-                        <Image src="/Footer/round-phone-24px.png" alt="Phone" width={24} height={24} className="shrink-0"/>
+                        <Image src="/Footer/round-phone-24px.png" alt="Phone" width={24} height={24}
+                               className="shrink-0"/>
                         <p className="footer-nav">(123) 456-7890</p>
                         <div className="mx-[60px]"></div>
-                        <Image src="/Footer/round-local-printshop-24px.png" alt="Fax" width={24} height={24} className="shrink-0"/>
+                        <Image src="/Footer/round-local-printshop-24px.png" alt="Fax" width={24} height={24}
+                               className="shrink-0"/>
                         <p className="footer-nav">(123) 456-7890</p>
                     </div>
                     <div className="flex items-center ml-1 mt-4 space-x-5">
                         <p className="footer-nav text-[#434138] mt-2">Social Media</p>
-                        <Image src="/Footer/facebook%20black.1.png" alt="Facebook" width={24} height={24} className="shrink-0"/>
+                        <Image src="/Footer/facebook%20black.1.png" alt="Facebook" width={24} height={24}
+                               className="shrink-0"/>
                         <Image src="/Footer/twitter.png" alt="Twitter" width={24} height={24} className="shrink-0"/>
-                        <Image src="/Footer/linkedin%20black.1.png" alt="LinkedIn" width={24} height={24} className="shrink-0"/>
-                        <Image src="/Footer/youtube%20color.1.png" alt="YouTube" width={24} height={24} className="shrink-0"/>
-                        <Image src="/Footer/instagram%20black.1.png" alt="Instagram" width={24} height={24} className="shrink-0"/>
-                        <Image src="/Footer/googleplus%20black.1.png" alt="Google+" width={24} height={24} className="shrink-0"/>
-                        <Image src="/Footer/pinterest%20color.1.png" alt="Pinterest" width={24} height={24} className="shrink-0"/>
+                        <Image src="/Footer/linkedin%20black.1.png" alt="LinkedIn" width={24} height={24}
+                               className="shrink-0"/>
+                        <Image src="/Footer/youtube%20color.1.png" alt="YouTube" width={24} height={24}
+                               className="shrink-0"/>
+                        <Image src="/Footer/instagram%20black.1.png" alt="Instagram" width={24} height={24}
+                               className="shrink-0"/>
+                        <Image src="/Footer/googleplus%20black.1.png" alt="Google+" width={24} height={24}
+                               className="shrink-0"/>
+                        <Image src="/Footer/pinterest%20color.1.png" alt="Pinterest" width={24} height={24}
+                               className="shrink-0"/>
                         <Image src="/Footer/rss%20black.1.png" alt="RSS" width={24} height={24} className="shrink-0"/>
                     </div>
                 </div>
