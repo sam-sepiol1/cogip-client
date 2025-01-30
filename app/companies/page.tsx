66e9778249586bd15e27c3a2c2f36f489d680705
KikiMaouw <cristelle.hermand@gmail.com>
2025-01-30 12:14:17 +0100

    Refacto naming

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Companies from "../components/Companies";
 
-export default function Invoices() {
+export default function Company() {
     return (
         <main>
             <Header />
+            <Companies />
+            <Footer />
         </main>
     );
 }
