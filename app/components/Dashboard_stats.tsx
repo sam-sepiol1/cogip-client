70a9672b1221a53836f4ca1243beb086c8738485
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-03 11:09:16 +0100

    FRONT : Add dashboard_stat.tsx

---
+++ app/components/Dashboard_stats.tsx
@@ -0,0 +1,34 @@
+export default function Daashboard_stats() {
+	const dataTest = {
+		nbInvoices: 245,
+		nbCompanies: 245,
+		nbContacts: 245,
+	};
+
+	return (
+		<main className='mt-16 ml-12 w-[507px] p-4 h-[232px] dashboard_drop-shadow'>
+			<h3 className='text-2xl font-bold mb-6'>Statistics</h3>
+			<ul className='flex flex-row gap-8 px-5'>
+				<li>
+					<div className='size-[70px] flex flex-col items-center justify-center text-white bg-[#4D4CAC] rounded-full'>
+						<span className='font-black text-xs'>{dataTest.nbInvoices}</span>
+						<span className='font-black text-xs'>Invoices</span>
+					</div>
+				</li>
+				<li>
+					<div className='size-[70px] flex flex-col items-center justify-center text-white bg-[#9698D6] rounded-full'>
+						<span className='font-black text-xs'>{dataTest.nbContacts}</span>
+						<span className='font-black text-xs'>Contacts</span>
+					</div>
+				</li>
+
+				<li>
+					<div className='size-[70px] flex flex-col items-center justify-center text-white bg-[#FF808B] rounded-full'>
+						<span className='font-black text-xs'>{dataTest.nbCompanies}</span>
+						<span className='font-black text-xs'>Companies</span>
+					</div>
+				</li>
+			</ul>
+		</main>
+	);
+}
