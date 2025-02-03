4e325773006c900c340dc3208277d0f4860d7937
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-03 15:31:42 +0100

    FRONT : Add dashboard menu

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,17 @@
-import Dashboard_header from "../components/Dashboard_header";
-import Dashboard_stats from "../components/Dashboard_stats";
+import Dashboard_header from '../components/Dashboard_header';
+import Dashboard_menu from '../components/Dashboard_menu';
+import Dashboard_stats from '../components/Dashboard_stats';
 
 export default function Dashboard() {
-    return (
-        <main>
-            <Dashboard_header />
-            <Dashboard_stats />
-        </main>
-    );
+	return (
+		<main>
+			<div className='flex'>
+				<Dashboard_menu />
+				<div className='flex flex-col w-full'>
+					<Dashboard_header />
+					<Dashboard_stats />
+				</div>
+			</div>
+		</main>
+	);
 }
