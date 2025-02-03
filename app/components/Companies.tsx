afb248efb7769e551212052665e312ccce7e55ef
KikiMaouw <cristelle.hermand@gmail.com>
2025-02-03 11:09:49 +0100

    Changed data to test sorting

--- app/components/Companies.tsx
+++ app/components/Companies.tsx
@@ -25,24 +25,24 @@ export default function Companies() {
         {
             id: 1,
             name: "fSociety",
-            tva: "123-456-7890",
-            country: "USA",
+            tva: "US456 654 321",
+            country: "United States",
             type: "Private",
             created: "02/12/2024"
         },
         {
             id: 2,
             name: "Heisenberg",
-            tva: "987-654-3210",
-            country: "USA",
+            tva: "BE0987 876 787",
+            country: "Belgium",
             type: "Client",
             created: "28/01/2025"
         },
         {
             id: 3,
             name: "Dunder Mifflin",
-            tva: "555-123-4567",
-            country: "USA",
+            tva: "FR 676 676 676",
+            country: "France",
             type: "Client",
             created: "16/05/2023"
         }
