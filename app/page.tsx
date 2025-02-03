df7b1101e29d983036821a677921c4a5f2b93bf4
KikiMaouw <cristelle.hermand@gmail.com>
2025-02-03 13:47:07 +0100

    Added images position

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,29 @@ import Invoices from "./components/Invoices";
 import Contacts from "./components/Contacts";
 import Companies from "./components/Companies";
 import Home_slogan from "./components/Home_slogan";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <Home_header />
-      <Invoices />
-      <Contacts />
-      <Companies />
-      <Home_slogan />
-      <Footer />
-    </main>
+      <main>
+          <Home_header/>
+          <div className="relative">
+              <Invoices/>
+              <div className="absolute right-0 mt-[-30px]">
+                  <Image src="/Home/Home-decoration_book.svg" alt="Book decoration"
+                         width={177} height={138}/>
+              </div>
+          </div>
+          <div className="relative">
+              <Contacts/>
+              <div className="absolute left-[-55px] top-full mt-[-30px] transform rotate-[30deg]">
+                  <Image src="/Home/e8f22fa8e9926850f515778113c009c2-2.png" alt="Lightbulb decoration"
+                         width={181} height={141.25}/>
+              </div>
+          </div>
+          <Companies/>
+          <Home_slogan/>
+          <Footer/>
+      </main>
   );
 }
