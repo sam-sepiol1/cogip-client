44e29a82fdba534b073b3f187aae070b57becf88
KikiMaouw <cristelle.hermand@gmail.com>
2025-02-03 17:54:42 +0100

    Insered <DataDisplayer />

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,12 +1,21 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import DatasDisplayer from "../components/DatasDisplayer";
 import Companies from "../components/Companies";
 
-export default function Company() {
+export default function CompaniesPage() {
     return (
         <main>
             <Header />
-            <Companies />
+
+            <DatasDisplayer title="All Companies" data={Companies.testData} columns={[
+                    { key: "name", label: "Name" },
+                    { key: "tva", label: "TVA" },
+                    { key: "country", label: "Country" },
+                    { key: "type", label: "Type" },
+                    { key: "created", label: "Created at" }
+                ]} searchPlaceholder="Search company" />
+
             <Footer />
         </main>
     );
--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,12 +1,21 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import DatasDisplayer from "../components/DatasDisplayer";
 import Contacts from "../components/Contacts";
 
-export default function Invoices() {
+export default function ContactsPage() {
     return (
         <main>
             <Header />
-            <Contacts />
+
+            <DatasDisplayer title="All Contacts" data={Contacts.testData} columns={[
+                    { key: "name", label: "Name" },
+                    { key: "phone", label: "Phone" },
+                    { key: "mail", label: "Mail" },
+                    { key: "company", label: "Company" },
+                    { key: "created", label: "Created at" }
+                ]} searchPlaceholder="Search contact" />
+
             <Footer />
         </main>
     );
