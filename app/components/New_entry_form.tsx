7c7ae726d727146799c94881d60f77a983041964
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-04 16:44:53 +0100

    FRONT : Add dashboard_contacts and New_entry_form component

---
+++ app/components/New_entry_form.tsx
@@ -0,0 +1,23 @@
+interface Props {
+    title: string;
+    fields: string[]
+}
+
+export default function New_entry_form({ title, fields }: Props) {
+    return (
+        <main className="flex flex-col ml-12 p-7 dashboard_form">
+            <h1 className="text-xl font-bold mb-12">{title}</h1>
+            <hr />
+            <form action="" method="post" className="flex flex-col w-full gap-10">
+                {fields.map((field) => {
+                    return (
+                        <div key={field} className="w-full">
+                            <input className="p-4 w-full font-light text-xl" type="text" name={field} id={field} placeholder={field} required />
+                        </div>
+                    );
+                })}
+                <button className="bg-[#9698D6] text-white p-4" type="submit">Save</button>
+            </form>
+        </main>
+    );
+}
