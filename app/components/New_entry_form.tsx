09095cf9de011c135221df7277e3280e7b9a7540
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-05 10:33:57 +0100

    FRONT : Add requested changes

--- app/components/New_entry_form.tsx
+++ app/components/New_entry_form.tsx
@@ -5,18 +5,18 @@ interface Props {
 
 export default function New_entry_form({ title, fields }: Props) {
     return (
-        <main className="flex flex-col ml-12 p-7 dashboard_form">
+        <main className="flex flex-col ml-12 p-7 dashboard_form mr-12 bg-white">
             <h1 className="text-xl font-bold mb-4">{title}</h1>
             <hr className="w-[95%] mb-12"/>
-            <form action="" method="post" className="flex flex-col w-full gap-10">
+            <form action="" method="post" className="flex flex-col gap-8">
                 {fields.map((field) => {
                     return (
-                        <div key={field} className="w-full">
-                            <input className="p-4 w-full font-light text-xl" type="text" name={field} id={field} placeholder={field} required />
+                        <div key={field} className="w-[95%]">
+                            <input className="dashboard_background p-4 w-full font-light text-xl" type="text" name={field} id={field} placeholder={field} required />
                         </div>
                     );
                 })}
-                <button className="bg-[#9698D6] text-white p-4" type="submit">Save</button>
+                <button className="bg-[#9698D6] text-white p-4 w-[95%]" type="submit">Save</button>
             </form>
         </main>
     );
--- app/dashboard_contacts/page.tsx
+++ app/dashboard_contacts/page.tsx
@@ -4,10 +4,10 @@ import New_entry_form from '../components/New_entry_form';
 
 export default function Dashboard_contacts() {
 	return (
-		<main>
+		<main className=''>
 			<div className='flex'>
 				<Dashboard_menu />
-				<div className='flex flex-col ml-[300px] w-full'>
+				<div className='flex flex-col ml-[300px] w-full dashboard_background h-screen justify-center '>
 					<Dashboard_header />
 					<New_entry_form title='New Contact' fields={['first_name', 'last_name', 'email', 'phone']} />
 				</div>
