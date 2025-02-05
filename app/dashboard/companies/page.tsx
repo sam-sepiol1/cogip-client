28dbb5c229c7ab8f14999bd75f5fcdd093578bb6
Neogiciaa <chevalier.stepheh@hotmail.com>
2025-02-05 12:12:38 +0100

    Fixed placeholders naming

--- app/dashboard/companies/page.tsx
+++ app/dashboard/companies/page.tsx
@@ -10,7 +10,7 @@ export default function Dashboard_companies() {
                 <Dashboard_menu />
                 <div className='flex flex-col ml-[300px] w-full dashboard_background h-full justify-center '>
                     <Dashboard_header />
-                    <New_entry_form title='New Company' fields={['companyName', 'tva', 'country', 'type']} />
+                    <New_entry_form title='New Company' fields={['Company name', 'TVA', 'Country', 'Type']} />
                 </div>
             </div>
         </main>
