43620012161e628925aea1a44f137d9c59fb0f47
KikiMaouw <cristelle.hermand@gmail.com>
2025-02-05 11:31:16 +0100

    Added space  after the section box

--- app/dashboard_invoices/page.tsx
+++ app/dashboard_invoices/page.tsx
@@ -11,22 +11,22 @@ export default function NewInvoicePage() {
 
                 <Dashboard_header />
 
-                <section className="bg-white p-8 rounded-[10px] dashboard_drop-shadow w-[1044px] h-[558px] border border-[#E0E0E0] ml-16">
+                <section className="bg-white p-8 rounded-[10px] dashboard_drop-shadow w-[1044px] h-[558px] border border-[#E0E0E0] ml-16 mb-28">
                     <h2 className="dashboard-font-title mt-6 mb-4">New invoice</h2>
 
                     <div className="w-[981px] h-[1px] bg-[#E0E0E0] mt-6 mb-10 mx-auto"></div>
 
-                    <form className="flex flex-col space-y-4">
-                        <input type="text" placeholder="Reference" className="dashboard-font-input w-full"/>
-                        <input type="number" placeholder="Price" className="dashboard-font-input w-full"/>
-                        <select className="dashboard-font-input w-full">
+                    <form className="flex flex-col space-y-10">
+                        <input type="text" placeholder="Reference" className="dashboard-font-input dashboard-input mt-5"/>
+                        <input type="number" placeholder="Price" className="dashboard-font-input dashboard-input"/>
+                        <select className="dashboard-font-input dashboard-input mb-8">
                             <option>Company name</option>
                             <option>fSociety</option>
                             <option>Heisenberg</option>
                             <option>Dunder Mifflin</option>
                         </select>
 
-                        <button type="submit" className="dashboard-font-button bg-[#9698D6]">Save</button>
+                        <button type="submit" className="dashboard-font-button h-[60px] bg-[#9698D6]">Save</button>
                     </form>
 
                 </section>
