e0ca4081d3c09487b250769654e4ab33050fa02b
KikiMaouw <cristelle.hermand@gmail.com>
2025-02-05 21:01:45 +0100

    Modified fake date for DB in company page

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,19 +1,36 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import DatasDisplayer from "../components/DatasDisplayer";
-import companiesData from "../components/Companies";
 
 export default function CompaniesPage() {
+    const [companies, setCompanies] = useState([]);
+
+    useEffect(() => {
+        const fetchCompanies = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000/api/company");
+                setCompanies(response.data);
+            } catch  {}
+        };
+
+        fetchCompanies();
+    }, []);
+
     return (
         <main>
             <Header />
 
-            <DatasDisplayer title="All Companies" data={companiesData} columns={[
+            <DatasDisplayer title="All Companies" data={ companies } columns={[
                     { key: "name", label: "Name" },
                     { key: "tva", label: "TVA" },
                     { key: "country", label: "Country" },
                     { key: "type", label: "Type" },
-                    { key: "created", label: "Created at" }
+                    { key: "created_at", label: "Created at" }
                 ]} searchPlaceholder="Search company" />
 
             <Footer />
