f22de59f5c5650c66d77ec51e6c5bf80b58f3859
Neogiciaa <chevalier.stepheh@hotmail.com>
2025-02-05 11:58:23 +0100

    Updated form

--- app/dashboard_invoices/page.tsx
+++ app/dashboard_invoices/page.tsx
@@ -1,5 +1,6 @@
 import Dashboard_menu from '../components/Dashboard_menu';
 import Dashboard_header from '../components/Dashboard_header';
+import New_entry_form from "@/app/components/New_entry_form";
 
 export default function NewInvoicePage() {
     return (
@@ -7,29 +8,11 @@ export default function NewInvoicePage() {
 
             <Dashboard_menu />
 
-            <div className="flex flex-col w-full ">
+            <div className="flex flex-col w-full ml-[300px]">
 
                 <Dashboard_header />
 
-                <section className="bg-white p-8 rounded-[10px] dashboard_drop-shadow w-[1044px] h-[558px] border border-[#E0E0E0] ml-16 mb-28">
-                    <h2 className="dashboard-font-title mt-6 mb-4">New invoice</h2>
-
-                    <div className="w-[981px] h-[1px] bg-[#E0E0E0] mt-6 mb-10 mx-auto"></div>
-
-                    <form className="flex flex-col space-y-10">
-                        <input type="text" placeholder="Reference" className="dashboard-font-input dashboard-input mt-5"/>
-                        <input type="number" placeholder="Price" className="dashboard-font-input dashboard-input"/>
-                        <select className="dashboard-font-input dashboard-input mb-8">
-                            <option>Company name</option>
-                            <option>fSociety</option>
-                            <option>Heisenberg</option>
-                            <option>Dunder Mifflin</option>
-                        </select>
-
-                        <button type="submit" className="dashboard-font-button h-[60px] bg-[#9698D6]">Save</button>
-                    </form>
-
-                </section>
+                <New_entry_form title='New Invoice' fields={['Reference', 'Price', 'Company name']} />
             </div>
         </main>
     );
