a3b56d9c6684ba05ed9d5116280750d0ece4d057
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-06 09:51:31 +0100

    FRONT : Add real data for stats component

--- app/components/Dashboard_stats.tsx
+++ app/components/Dashboard_stats.tsx
@@ -1,9 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Daashboard_stats() {
-	const dataTest = {
-		nbInvoices: 245,
-		nbCompanies: 245,
-		nbContacts: 245,
-	};
+	const [nbInvoices, setNbInvoices] = useState(0);
+	const [nbCompanies, setNbCompanies] = useState(0);
+	const [nbContacts, setNbContacts] = useState(0);
+
+	const fetchData = async () => {
+		try {
+			const invoicesResponse = await axios.get("http://localhost:3000/api/countInvoices");
+			const companiesResponse = await axios.get("http://localhost:3000/api/countCompanies");
+			const contactsResponse = await axios.get("http://localhost:3000/api/countContacts");
+			setNbInvoices(invoicesResponse.data.totalInvoices);
+			setNbCompanies(companiesResponse.data.totalCompanies);
+			setNbContacts(contactsResponse.data.totalContacts);
+		}
+		catch (error) {
+			console.error("Error fetching data:", error);
+		}
+			
+	}
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
 
 	return (
 		<main className='dashboard-stats w-[507px] px-4 h-[232px] dashboard_drop-shadow'>
@@ -11,20 +34,20 @@ export default function Daashboard_stats() {
 			<ul className='flex flex-row justify-between  px-5'>
 				<li>
 					<div className='size-[80px] flex flex-col items-center justify-center text-white bg-[#4D4CAC] rounded-full'>
-						<span className='font-black text-xs'>{dataTest.nbInvoices}</span>
+						<span className='font-black text-xs'>{nbInvoices}</span>
 						<span className='font-black text-xs'>Invoices</span>
 					</div>
 				</li>
 				<li>
 					<div className='size-[80px] flex flex-col items-center justify-center text-white bg-[#9698D6] rounded-full'>
-						<span className='font-black text-xs'>{dataTest.nbContacts}</span>
+						<span className='font-black text-xs'>{nbContacts}</span>
 						<span className='font-black text-xs'>Contacts</span>
 					</div>
 				</li>
 
 				<li>
 					<div className='size-[80px] flex flex-col items-center justify-center text-white bg-[#FF808B] rounded-full'>
-						<span className='font-black text-xs'>{dataTest.nbCompanies}</span>
+						<span className='font-black text-xs'>{nbCompanies}</span>
 						<span className='font-black text-xs'>Companies</span>
 					</div>
 				</li>
