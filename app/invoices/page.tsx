3d3c1b716b5e8933142f1772b2cafc76c31958aa
KikiMaouw <cristelle.hermand@gmail.com>
2025-02-06 10:46:53 +0100

    Refacto for added the companies name in invoice

--- app/invoices/page.tsx
+++ app/invoices/page.tsx
@@ -1,24 +1,40 @@
-'use client'
+'use client';
 
 import { useEffect, useState } from "react";
 import axios from "axios";
+
 import Header from "../components/Header";
 import DatasDisplayer from "../components/DatasDisplayer";
 import Footer from "../components/Footer";
 
+interface Invoice {
+    id: number;
+    ref: string;
+    due_date: string;
+    id_company: number;
+    created_at: string;
+    company: string;
+}
+
 export default function InvoicePage() {
-    const [invoices, setInvoices] = useState([]);
+    const [invoices, setInvoices] = useState<Invoice[]>([]);
+    const limit = 10;
+    const offset = 0;
 
     useEffect(() => {
         const fetchInvoices = async () => {
             try {
-                const response = await axios.get("http://localhost:3000/api/invoice");
+                console.log("Retrieving invoices...");
+                const response = await axios.get<Invoice[]>(`http://localhost:3000/api/paginatedInvoices/${limit}/${offset}`);
+                console.log("Invoices recovered :", response.data);
                 setInvoices(response.data);
-            } catch  {}
+            } catch (error) {
+                console.error("Error retrieving invoices :", error);
+            }
         };
 
         fetchInvoices();
-    }, []);
+    }, [limit, offset]);
 
     return (
         <main>
@@ -30,7 +46,7 @@ export default function InvoicePage() {
                 columns={[
                     { key: "ref", label: "Invoice Number" },
                     { key: "due_date", label: "Due Date" },
-                    { key: "id_company", label: "Company" },
+                    { key: "company", label: "Company" },
                     { key: "created_at", label: "Created at" }
                 ]}
                 searchPlaceholder="Search invoice"
