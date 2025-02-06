bd3f32c27b022f62733a440d15175ddf33718ff3
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-06 09:56:10 +0100

    FRONT : add better props to the form

--- app/components/New_entry_form.tsx
+++ app/components/New_entry_form.tsx
@@ -1,22 +1,27 @@
+import React from "react";
+
 interface Props {
     title: string;
-    fields: string[]
+    fields: string[];
+    submitText: string;
+    onChange: (name : string, value : string) => void;
+    onSubmit: (e : React.FormEvent<HTMLFormElement>) => void;
 }
 
-export default function New_entry_form({ title, fields }: Props) {
+export default function New_entry_form({ title, fields, onChange, onSubmit, submitText }: Props) {
     return (
-        <main className="flex flex-col justify-center ml-12 p-7 dashboard_form mr-12 bg-white rounded-lg mb-14 ">
+        <main className="flex flex-col justify-center items-center w-full p-7 dashboard_form mr-12 bg-white rounded-lg mb-14 ">
             <h1 className="text-xl font-bold mb-4">{title}</h1>
             <hr className="w-[95%] mb-12"/>
-            <form action="" method="post" className="flex flex-col gap-8 ">
+            <form action="" method="post" className="flex flex-col gap-8 " onSubmit={onSubmit}>
                 {fields.map((field) => {
                     return (
                         <div key={field} className="w-[95%]">
-                            <input className="dashboard_background p-4 w-full font-light text-xl rounded-md" type="text" name={field} id={field} placeholder={field} required />
+                            <input className="dashboard_background p-4 w-[650px] font-light text-xl rounded-md" type="text" name={field} id={field} placeholder={field} onChange={(e => onChange(field.toLowerCase(), e.target.value))}  required />
                         </div>
                     );
                 })}
-                <button className="bg-[#9698D6] text-white p-4 w-[35%] self-center" type="submit">Save</button>
+                <button className="bg-[#9698D6] text-white p-4 w-[35%] self-center" type="submit" >{submitText}</button>
             </form>
         </main>
     );
