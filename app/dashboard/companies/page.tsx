6bbbd3c50cfa99f459aba639be15f3a7661c6a7d
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-07 15:38:49 +0100

    Add handleChange and Submit logic

--- app/dashboard/companies/page.tsx
+++ app/dashboard/companies/page.tsx
@@ -1,28 +1,56 @@
+'use client';
+import { useState } from 'react';
+import axios from 'axios';
+
 import Dashboard_menu from '../../components/Dashboard_menu';
 import Dashboard_header from '../../components/Dashboard_header';
 import New_entry_form from '../../components/New_entry_form';
 
 export default function Dashboard_companies() {
-    // @ts-ignore
-    function handleChange = () => {
-        console.log("Coucou")
-    }
+	const [formData, setFormData] = useState({
+		companyName: '',
+		TVA: '',
+		country: '',
+		type: '',
+	});
 
-    // @ts-ignore
-    function handleSubmit = () => {
-        console.log("Coucou")
-    }
+	const handleChange = (name: string, value: string) => {
+        setFormData({
+            ...formData,
+			[name]: value,
+		});
+        console.log(formData);
+        
+	};
 
-    return (
-        <main className='h-screen'>
-            <div className='flex h-full'>
-                <Dashboard_menu />
-                <div className='flex flex-col ml-[300px] w-full dashboard_background h-full justify-center '>
-                    <Dashboard_header />
-                    <New_entry_form title='New Company' fields={['Company name', 'TVA', 'Country', 'Type']}
-                                    submitText='Create company' onChange={handleChange} onSubmit={handleSubmit} placeholders={[]} errorMessage={''} />
-                </div>
-            </div>
-        </main>
-    );
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+		console.log('Formulaire soumis avec : ', formData);
+
+		try {
+			const response = await axios.post("http://localhost:3000/api/company", formData);
+			console.log('Entreprise créée avec success :', response.data);
+		} catch (error) {
+			console.error('Erreur lors de la création :', error);
+		}
+	};
+	return (
+		<main className='h-screen'>
+			<div className='flex h-full'>
+				<Dashboard_menu />
+				<div className='flex flex-col ml-[300px] w-full dashboard_background h-full justify-center '>
+					<Dashboard_header />
+					<New_entry_form
+						title='New Company'
+						fields={['companyName', 'TVA', 'country', 'type']}
+						submitText='Create company'
+						onChange={handleChange}
+						onSubmit={handleSubmit}
+						placeholders={['Company name', 'TVA', 'Country', 'Type']}
+						errorMessage={''}
+					/>
+				</div>
+			</div>
+		</main>
+	);
 }
