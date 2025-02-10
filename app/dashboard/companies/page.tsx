99efddf3efe9c1d2fdcc7bc5eb0a72f505ce6fe8
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-10 12:23:31 +0100

    Fix CSS class to match figma

--- app/dashboard/companies/page.tsx
+++ app/dashboard/companies/page.tsx
@@ -1,78 +1,86 @@
 'use client';
 import { useState } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 import Dashboard_menu from '../../components/Dashboard_menu';
 import Dashboard_header from '../../components/Dashboard_header';
 import New_entry_form from '../../components/New_entry_form';
 
 export default function Dashboard_companies() {
-    const companyTypes: Record<string, number> = {
-        'sarl': 1,
-        'sa': 2,
-        'auto-entrepreneur': 3,
-        'association': 4,
-        'startup': 5,
-    };
+	const router = useRouter();
 
-    const [formData, setFormData] = useState({
-        companyName: '',
-        tva: '',
-        country: '',
-        type: '',
-    });
+	const companyTypes: Record<string, number> = {
+		'sarl': 1,
+		'sa': 2,
+		'auto-entrepreneur': 3,
+		'association': 4,
+		'startup': 5,
+	};
 
-    const [errorMessage, setErrorMessage] = useState('');
+	const [formData, setFormData] = useState({
+		companyName: '',
+		tva: '',
+		country: '',
+		type: '',
+	});
 
-    const handleChange = (name: string, value: string) => {
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
+	const [errorMessage, setErrorMessage] = useState('');
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+	const handleChange = (name: string, value: string) => {
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
 
-        const typeId = companyTypes[formData.type];
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
 
-        if (!typeId) {
-            setErrorMessage('Please select a valid company type: SARL, SA, Auto-entrepreneur, Association, or Startup');
-            return;
-        }
+		const typeId = companyTypes[formData.type];
 
-        try {
-            const response = await axios.post('http://localhost:3000/api/company', {
-                name: formData.companyName,
-                tva: formData.tva,
-                country: formData.country,
-                type_id: typeId,
-            });
-            console.log('Company created :', response.data);
-            setErrorMessage(''); // Clear error message on success
-        } catch (error) {
-            console.error('Error during creation:', error);
-            setErrorMessage('Error during creation. Please try again.');
-        }
-    };
+		if (!typeId) {
+			setErrorMessage('Please select a valid company type: SARL, SA, Auto-entrepreneur, Association, or Startup');
+			return;
+		}
 
-    return (
-        <main className='h-screen'>
-            <div className='flex h-full'>
-                <Dashboard_menu />
-                <div className='flex flex-col ml-[300px] w-full dashboard_background h-full justify-center '>
-                    <Dashboard_header />
-                    <New_entry_form
-                        title='New Company'
-                        fields={['companyName', 'tva', 'country', 'type']}
-                        submitText='Create company'
-                        onChange={handleChange}
-                        onSubmit={handleSubmit}
-                        placeholders={['Company name', 'TVA', 'Country', 'Type']}
-                        errorMessage={errorMessage}
-                    />
-                </div>
-            </div>
-        </main>
-    );
+		try {
+			const response = await axios.post('http://localhost:3000/api/company', {
+				name: formData.companyName,
+				tva: formData.tva,
+				country: formData.country,
+				type_id: typeId,
+			});
+
+			if (response.status === 201) {
+				router.push('/dashboard');
+			}
+
+			console.log('Company created :', response.data);
+			setErrorMessage('');
+		} catch (error) {
+			console.error('Error during creation:', error);
+			setErrorMessage('Error during creation. Please try again.');
+		}
+	};
+
+	return (
+		<main className=''>
+			<div className='flex'>
+				<Dashboard_menu />
+				<div className='flex flex-col ml-[300px] w-full dashboard_background h-screen justify-center '>
+					<Dashboard_header />
+					<New_entry_form
+						title='New Company'
+						fields={['companyName', 'tva', 'country', 'type']}
+						submitText='Create company'
+						onChange={handleChange}
+						onSubmit={handleSubmit}
+						placeholders={['Company name', 'TVA', 'Country', 'Type']}
+						errorMessage={errorMessage}
+					/>
+				</div>
+			</div>
+		</main>
+	);
 }
