88a8a1115e2b3952837bda5cb2c39294bfaf33c3
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-10 10:37:08 +0100

    Add logic to add new invoice [STILL IN DEV]

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useState } from 'react';
+import axios from 'axios';
+
+import Dashboard_header from '../../components/Dashboard_header';
+import Dashboard_menu from '../../components/Dashboard_menu';
+import New_entry_form from '../../components/New_entry_form';
+
+export default function Dashboard_contacts() {
+	const [formData, setFormData] = useState({
+        company: '',
+        price: '',
+	});
+
+	const handleChange = (name: string, value: string) => {
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+		console.log(formData);
+		
+	};
+
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+		
+		const companyResponse = await axios.get(`http://localhost:3000/api/searchCompany/${formData.company}`);		
+		const company_id = companyResponse.data[0].id;
+		try {
+			const response = await axios.post('http://localhost:3000/api/invoice', {
+				company_id: company_id,
+				price: formData.price,
+			});
+			console.log('Invoice created :', response.data);
+		} catch (error) {
+			console.error('Error during creation:', error);
+		}
+	};
+
+	return (
+		<main className=''>
+			<div className='flex'>
+				<Dashboard_menu />
+				<div className='flex flex-col ml-[300px] w-full dashboard_background h-screen justify-center '>
+					<Dashboard_header />
+					<New_entry_form
+						title='New Invoice'
+						fields={['company', 'price']}
+						submitText='Create Invoice'
+						onChange={handleChange}
+						onSubmit={handleSubmit}
+						placeholders={['Company', 'Price']}
+						errorMessage={''}
+					/>
+				</div>
+			</div>
+		</main>
+	);
+}
