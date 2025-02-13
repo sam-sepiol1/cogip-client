9666d1c5fa10317038934826fe47906ee48c9e01
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2025-02-13 16:11:34 +0100

    Add jwt-decode dependency and refactor user state management in dashboard components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,59 +1,55 @@
 'use client';
 
-import Login_register_header from "@/app/components/Login_register_header";
-import New_entry_form from "@/app/components/New_entry_form";
-import React, { useState } from "react";
-import axios from "axios";
-import { useRouter } from "next/navigation";
+import Login_register_header from '@/app/components/Login_register_header';
+import New_entry_form from '@/app/components/New_entry_form';
+import { useState } from 'react';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
 
 export default function Login() {
-    const [error, setError] = useState("");
-    const router = useRouter();
+	const [error, setError] = useState('');
 
-    const [dto, setDto] = useState({
-        email: '',
-        password: ''
-    });
+	const [dto, setDto] = useState({
+		email: '',
+		password: '',
+	});
 
-    const handleChange = (name: string, value: string) => {
-        setDto((prevDto) => ({
-            ...prevDto,
-            [name]: value
-        }));
-    };
+	const handleChange = (name: string, value: string) => {
+		setDto((prevDto) => ({
+			...prevDto,
+			[name]: value,
+		}));
+	};
 
-    const handleSubmitForm = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setError("");
+	const handleSubmitForm = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setError('');
 
-        try {
-            const response = await axios.post('http://localhost:3000/api/auth/login', dto);
+		try {
+			const response = await axios.post('http://localhost:3000/api/auth/login', dto);
 
-            console.log("Login success:", response.data);
-            router.push('/dashboard');
+			const token = response.data.data.token;
+			localStorage.setItem('token', token);
+			
+            const decodedToken = jwtDecode<{ user: string }>(token);
+			const userData = decodedToken.user;
+			localStorage.setItem('user', JSON.stringify(userData));
 
-        } catch (error: any) {
-            console.error("Error while login:", error);
-            if (error.response) {
-                setError(error.response.data.message || "Login failed, please try again.");
-            } else {
-                setError("An unexpected error occurred. Please check your connection.");
-            }
-        }
-    };
+			window.location.href = '/dashboard';
+		} catch (error: unknown) {
+			console.error('Error while login:', error);
+			if (axios.isAxiosError(error) && error.response) {
+				setError(error.response.data.message || 'Login failed, please try again.');
+			} else {
+				setError('An unexpected error occurred. Please check your connection.');
+			}
+		}
+	};
 
-    return (
-        <main>
-            <Login_register_header />
-            <New_entry_form
-                title="Please, Login to continue."
-                fields={['email', 'password']}
-                onChange={handleChange}
-                onSubmit={handleSubmitForm}
-                submitText="Login"
-                placeholders={['Email', 'Password']}
-                errorMessage={error}
-            />
-        </main>
-    );
+	return (
+		<main>
+			<Login_register_header />
+			<New_entry_form title='Please, Login to continue.' fields={['email', 'password']} onChange={handleChange} onSubmit={handleSubmitForm} submitText='Login' placeholders={['Email', 'Password']} errorMessage={error} formData={dto} />
+		</main>
+	);
 }
